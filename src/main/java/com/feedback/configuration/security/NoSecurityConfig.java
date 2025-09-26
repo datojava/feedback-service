0c34c52ea533f7ea13d5976bc837ef2d5cbca6df
LetCryptoAdmin <admin@letcrypto.net>
2025-09-26 19:04:56 +0400

    Added Log4j2

--- src/main/java/com/feedback/configuration/security/NoSecurityConfig.java
+++ src/main/java/com/feedback/configuration/security/NoSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.feedback.configuration.security;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -7,6 +8,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Log4j2
 @Configuration
 public class NoSecurityConfig {
 
